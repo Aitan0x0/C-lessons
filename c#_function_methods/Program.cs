1d6c6b31dadb5067a350e12f1340040525690952
Aitan0x0 <aytanqurbanova00@gmail.com>
2026-03-08 22:33:35 +0400

    PrintSimvol funk

---
+++ c#_function_methods/Program.cs
@@ -0,0 +1,40 @@
+﻿//istifadecinin daxil etdiyi say qeder simvol yazan kod
+
+
+namespace c__function_methods
+{
+    internal class Program
+    {
+
+
+        static void PrintSimvols(int count , char simvol)
+        {
+            for (int i = 0; i <count; i++)
+            {
+                Console.Write(simvol);
+            }
+
+
+
+        }
+        static void Main(string[] args)
+        {
+
+
+            int charCount = 0;
+            char simvol;
+
+            Console.WriteLine("Yazilacaq simvolu ve sayini daxil edin:");
+            Console.Write("Simvol:");
+            simvol = char.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Simvol sayi:");
+            charCount = int.Parse(Console.ReadLine());
+
+            PrintSimvols(charCount, simvol);
+
+
+
+        }
+    }
+}
