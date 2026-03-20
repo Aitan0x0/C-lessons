574d9acdfb696c1329a9f1d6ced2848de8a67c57
Aitan0x0 <aytanqurbanova00@gmail.com>
2026-03-21 01:15:05 +0400

    Unikal reqem saxlayan massiv kodunun file'ni yaratdim

---
+++ Massive metods/Unikal reqemler saxlayan massiv/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Unikal_reqemler_saxlayan_massiv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
